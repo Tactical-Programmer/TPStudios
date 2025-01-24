32551c50aa95d5d6e12845c5e0d911cd41494d7f
Tactical_Programmer <tacticalprogrammerstudios@gmail.com>
2025-01-24 20:24:15 +0000

    Added Background Color to Mini-Websites

--- src/MiniWebsites/demon-shuriken-master/DemonShurikenMaster.jsx
+++ src/MiniWebsites/demon-shuriken-master/DemonShurikenMaster.jsx
@@ -2,7 +2,7 @@ import "./DemonShurikenMaster.css";
 
 function MiniWebsiteDemonShurikenMaster() {
   return (
-    <div>
+    <div className="DSM-container">
       <h1>Demon Shuriken Master</h1>
       <p className="DSM-p">Welcome to the mini website for Demon Shuriken Master.</p>
       {/* Add more content as needed */}
