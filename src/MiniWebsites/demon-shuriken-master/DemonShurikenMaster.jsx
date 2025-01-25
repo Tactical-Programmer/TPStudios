b44fd43fffd51fa73b440f72120bfbfd29d0ca02
Tactical_Programmer <tacticalprogrammerstudios@gmail.com>
2025-01-25 13:41:05 +0000

    Added Background Music to Mini Websites

--- src/MiniWebsites/demon-shuriken-master/DemonShurikenMaster.jsx
+++ src/MiniWebsites/demon-shuriken-master/DemonShurikenMaster.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState, useRef } from "react";
 import "./DemonShurikenMaster.css";
+import BackgroundMusic from '../../Music/DemonShurikenMaster.mp3';
 
 function MiniWebsiteDemonShurikenMaster() {
+  const BackgroundMusicAudio = useRef(new Audio(BackgroundMusic));
+  BackgroundMusicAudio.current.loop = true;
+
+  const [isMuted, setIsMuted] = useState(false);
+
+  useEffect(() => {
+    BackgroundMusicAudio.current.currentTime = 0;
+    BackgroundMusicAudio.current.volume = 0.3;
+    BackgroundMusicAudio.current.play();
+
+    return () => {
+      BackgroundMusicAudio.current.pause();
+    };
+  }, []);
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+    BackgroundMusicAudio.current.muted = !isMuted;
+    if (!isMuted) {
+      BackgroundMusicAudio.current.play(); // Ensure playback resumes when unmuted
+    }
+  };
+
   return (
     <div className="DSM-container">
       <h1>Demon Shuriken Master</h1>
       <p className="DSM-p">Welcome to the mini website for Demon Shuriken Master.</p>
+      <button onClick={toggleMute} className="mute-button">
+        {isMuted ? "Unmute" : "Mute"}
+      </button>
       {/* Add more content as needed */}
     </div>
   );
--- src/MiniWebsites/midnight-drive/MidnightDrive.jsx
+++ src/MiniWebsites/midnight-drive/MidnightDrive.jsx
@@ -1,10 +1,38 @@
+import { useEffect, useState, useRef } from "react";
 import "./MidnightDrive.css";
+import BackgroundMusic from '../../Music/MidnightDrive.mp3';
 
 function MiniWebsiteMidnightDrive() {
+  const BackgroundMusicAudio = useRef(new Audio(BackgroundMusic));
+  BackgroundMusicAudio.current.loop = true;
+
+  const [isMuted, setIsMuted] = useState(false);
+
+  useEffect(() => {
+    BackgroundMusicAudio.current.currentTime = 0;
+    BackgroundMusicAudio.current.volume = 0.15;
+    BackgroundMusicAudio.current.play();
+
+    return () => {
+      BackgroundMusicAudio.current.pause();
+    };
+  }, []);
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+    BackgroundMusicAudio.current.muted = !isMuted;
+    if (!isMuted) {
+      BackgroundMusicAudio.current.play(); // Ensure playback resumes when unmuted
+    }
+  };
+
   return (
     <div className="MD-container">
       <h1>Midnight Drive</h1>
       <p className="MD-p">Welcome to the mini website for Midnight Drive.</p>
+      <button onClick={toggleMute} className="mute-button">
+        {isMuted ? "Unmute" : "Mute"}
+      </button>
       {/* Add more content as needed */}
     </div>
   );
